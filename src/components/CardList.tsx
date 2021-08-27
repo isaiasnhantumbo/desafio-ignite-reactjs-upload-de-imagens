4817fe88fa2e1a2b43bd8373d9605ec62bb32bef
Isaias Nhantumbo <isaiasnhantumbo54@gmail.com>
2021-08-27 17:54:13 +0200

    :sparkles: feat:show Images and modal

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -16,17 +16,27 @@ interface CardsProps {
 }
 
 export function CardList({ cards }: CardsProps): JSX.Element {
-  // TODO MODAL USEDISCLOSURE
+  const [currentImageUrl, setCurrentImageUrl] = useState('');
+  const { isOpen, onClose, onOpen } = useDisclosure();
 
-  // TODO SELECTED IMAGE URL STATE
-
-  // TODO FUNCTION HANDLE VIEW IMAGE
+  function handleViewImage(imageUrl: string): void {
+    onOpen();
+    setCurrentImageUrl(imageUrl);
+  }
 
   return (
     <>
-      {/* TODO CARD GRID */}
+      <SimpleGrid spacing={10} columns={[2, 2, 3]}>
+        {cards.map(card => {
+          return <Card key={card.id} data={card} viewImage={handleViewImage} />;
+        })}
+      </SimpleGrid>
 
-      {/* TODO MODALVIEWIMAGE */}
+      <ModalViewImage
+        isOpen={isOpen}
+        imgUrl={currentImageUrl}
+        onClose={onClose}
+      />
     </>
   );
 }
