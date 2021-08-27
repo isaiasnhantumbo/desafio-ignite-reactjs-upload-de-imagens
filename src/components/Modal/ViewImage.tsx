13da0c55657248712a42ff91eed635e9716c351f
Isaias Nhantumbo <isaiasnhantumbo54@gmail.com>
2021-08-27 17:53:45 +0200

    :sparkles: feat: Image view modal

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -19,5 +19,36 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      size="xl"
+      isCentered
+      motionPreset="scale"
+    >
+      <ModalOverlay />
+      <ModalContent
+        mx="auto"
+        h="auto"
+        w="auto"
+        maxW={['300px', '500px', '900px']}
+        maxH={['350px', '450px', '600px']}
+      >
+        <ModalBody p="0">
+          <Image
+            src={imgUrl}
+            maxW={['300px', '500px', '900px']}
+            maxH={['350px', '450px', '600px']}
+          />
+        </ModalBody>
+
+        <ModalFooter bg="pGray.800" borderBottomRadius="6px" p={0}>
+          <Link href={imgUrl} isExternal fontSize="sm" mr="auto" py={2} ml={2}>
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
